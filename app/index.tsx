ac8248e02f2b9650e69554ab1b254673f9e33fa6
Lorenzo <lorenzo.podio@edu.itspiemonte.it>
2024-10-01 15:37:38 +0200

    Aggiustato, overall uguale

--- app/index.tsx
+++ app/index.tsx
@@ -1,67 +1,66 @@
-import { Text, View, FlatList, ListRenderItem, Button } from "react-native";
-import { Card, cards } from "@/api/data.mock";
-import { CardComponent } from "@/components/molecules/cardComponent/cardComponent.molecule";
+import { Text, View, FlatList, ListRenderItem, Button } from 'react-native';
+import { Card, cards } from '@/api/data.mock';
+import { CardComponent } from '@/components/molecules/cardComponent/cardComponent.molecule';
 import { useState } from 'react';
-import { ButtonComponent } from "@/components/atoms/button/button.atom";
-import {SafeAreaView, StyleSheet, TextInput} from 'react-native';
+import { ButtonComponent } from '@/components/atoms/button/button.atom';
+import { SafeAreaView, StyleSheet, TextInput } from 'react-native';
 
 export default function Index() {
-const [counter, setCounter] = useState(0);
-const [tempform, setTempForm] = useState("");
-const [form, setForm] = useState("");
+  const [counter, setCounter] = useState(0);
+  const [tempform, setTempForm] = useState('');
+  const [form, setForm] = useState('');
 
-// CALLBACKS //
+  // CALLBACKS //
 
-const onPressCounter = () => {
-  setCounter((prevState) => {
-    return prevState + 1;
-  })
-};
+  const onPressCounter = () => {
+    setCounter((prevState) => {
+      return prevState + 1;
+    });
+  };
 
+  const onPressPrintForm = () => {
+    setForm(tempform);
+  };
 
-const onPressPrintForm = () => {
-  setForm(tempform);
-}
+  const onPressResetForm = () => {
+    setForm('');
+  };
 
-const onPressResetForm = () => {
-  setForm("");
-}
+  // UI //
+  const renderItem: ListRenderItem<Card> = ({ item, index }) => {
+    return (
+      <CardComponent
+        key={index}
+        title={item.title}
+        subTitle={item.subTitle}
+        backgroundColor={item.backgroundColor}
+        image={item.image}
+      />
+    );
+  };
+  const ItemSeparatorComponent = () => <View style={{ height: 16 }} />;
+  const ListHeaderComponent = () => {
+    return (
+      <Text style={{ fontSize: 24, paddingVertical: 16, textAlign: 'center' }}>
+        Le card di oggi:{' '}
+      </Text>
+    );
+  };
+  const ListFooterComponent = () => {
+    return (
+      <Text style={{ fontSize: 24, paddingVertical: 16, textAlign: 'center' }}>
+        Fine della lista
+      </Text>
+    );
+  };
 
-// UI //
-const renderItem: ListRenderItem<Card> = ({ item, index }) => {
-  return (
-    <CardComponent
-      key={index}
-      title={item.title}
-      subTitle={item.subTitle}
-      backgroundColor={item.backgroundColor}
-      image={item.image}
-    />
-  );
-};
-const ItemSeparatorComponent = () => <View style={{ height: 16 }} />;
-const ListHeaderComponent = () => {
-  return (
-    <Text style={{ fontSize: 24, paddingVertical: 16, textAlign: "center" }}>
-      Le card di oggi:{" "}
-    </Text>
-  );
-};
-const ListFooterComponent = () => {
-  return (
-    <Text style={{ fontSize: 24, paddingVertical: 16, textAlign: "center" }}>
-      Fine della lista
-    </Text>
-  );
-};
-
-const ListEmptyComponent = () => {
-  return (
-    <Text style={{ fontSize: 24, paddingVertical: 16, textAlign: "center" }}>
-      Nessuna card da mostrare
-    </Text>
-  );
-};
+  const ListEmptyComponent = () => {
+    return (
+      <Text style={{ fontSize: 24, paddingVertical: 16, textAlign: 'center' }}>
+        Nessuna card da mostrare
+      </Text>
+    );
+  };
 
   return (
     /*
@@ -77,13 +76,14 @@ const ListEmptyComponent = () => {
     /n>
     */
 
-    <View style={{
-      padding: 20,
-      backgroundColor: '#f5f5f5',
-      borderRadius: 10,
-      alignContent:"center",
-      justifyContent:"center"
-    }}>
+    <View
+      style={{
+        padding: 20,
+        backgroundColor: '#f5f5f5',
+        borderRadius: 10,
+        alignContent: 'center',
+        justifyContent: 'center',
+      }}>
       <SafeAreaView>
         <TextInput
           style={styles.input}
@@ -91,26 +91,30 @@ const ListEmptyComponent = () => {
           onChangeText={(text: string) => setTempForm(text)}
         />
       </SafeAreaView>
-      <Text style={{
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 15,
-        color: '#333',
-      }}>Testo che hai scritto: {form}</Text>
-      <ButtonComponent 
-        title="Invia" 
-        onPress={onPressPrintForm} 
+      <Text
+        style={{
+          fontSize: 24,
+          fontWeight: 'bold',
+          marginBottom: 15,
+          color: '#333',
+        }}>
+        Testo: {form}
+      </Text>
+      <ButtonComponent
+        title="Invia"
+        onPress={onPressPrintForm}
         disabled={false}
-        style={{margin: 20, alignContent:"center"}}
+        style={{ margin: 20, alignContent: 'center' }}
       />
-      <ButtonComponent 
-        title="Reset" 
-        onPress={onPressResetForm} 
+      <ButtonComponent
+        title="Reset"
+        onPress={onPressResetForm}
         disabled={false}
+        style={{ backgroundColor: 'red' }}
       />
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   input: {
