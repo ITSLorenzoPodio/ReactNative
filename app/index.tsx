e40a0e288741d87ea586f37319be53d59fb6526d
Lorenzo <lorenzo.podio@edu.itspiemonte.it>
2024-10-22 14:25:49 +0200

    UseState, useMemo, callBack

--- app/index.tsx
+++ app/index.tsx
@@ -1,37 +1,78 @@
-import { Text, View } from 'react-native';
+import { Button, Text, View } from 'react-native';
 import { ButtonComponent } from '@/components/atoms/button/button.atom';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { TextInputComponent } from '@/components/atoms/textInput/textInput.atom';
+import { useMemo } from 'react';
+import { TextInput } from 'react-native-gesture-handler';
 
 export default function Index() {
+  const [quantity, setQuantity] = useState(1);
   const [text, setText] = useState('');
-  const [isConfirmPressed, setIsConfirmPressed] = useState(false);
+  const [shownText, setShownText] = useState('');
+
+  useEffect(() => {
+    alert('Benvenuto');
+  }, []);
+
+  useEffect(() => {
+    setShownText(text);
+  }, [text]);
+
+  const onChangedText = (newText: string) => {
+    setText(newText);
+  };
+
+  //useMemo
+  const productPrice = useMemo(() => {
+    return {
+      price: 30,
+      quantity: quantity,
+      discount: 10,
+      additionalPrice: 5,
+    };
+  }, [quantity]);
 
   // ** CALLBACKS  ** //
-  const onChangeText = (text: string) => setText(text);
-  const onPress = () => {
-    setIsConfirmPressed(true);
+  const onPressAddOne = () => {
+    setQuantity((prevState) => prevState + 1);
   };
 
-  const onReset = () => {
-    setText('');
-    setIsConfirmPressed(false);
+  const onPressRemoveOne = () => {
+    setQuantity((prevState) => (prevState == 1 ? 1 : prevState - 1));
   };
+
+  const calculatePrice = useMemo(() => {
+    return {
+      totalPrice:
+        (productPrice.price - productPrice.discount) * quantity + productPrice.additionalPrice,
+    };
+  }, [quantity]);
+
+  const incrementQuantity = useCallback(onPressAddOne, []);
+  const decrementQuantity = useCallback(onPressRemoveOne, []);
 
   // ** UI  ** //
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <TextInputComponent value={text} onChangeText={onChangeText} />
-      {isConfirmPressed ? (
-        <Text style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}>{text}</Text>
-      ) : null}
-      <Text style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}></Text>
-      <ButtonComponent title="Cliccami" onPress={onPress} />
-      <ButtonComponent
-        style={{ marginTop: 16, backgroundColor: 'red' }}
-        title="Reset"
-        onPress={onReset}
-      />
+    <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
+      <Text style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}>
+        Price: ${productPrice.price}
+        {'\n'}
+        Discount: ${productPrice.discount}
+        {'\n'}
+        Quantity: {productPrice.quantity}
+        {'\n'}
+        Additional Price: ${productPrice.additionalPrice}
+        {'\n'}
+        Total: ${calculatePrice.totalPrice}
+      </Text>
+      <TextInput
+        style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}
+        value={text}
+        onChangeText={onChangedText}
+        placeholder="ciao"></TextInput>
+      <Text style={{ fontSize: 18, paddingVertical: 32, textAlign: 'center' }}>{shownText}</Text>
+      <ButtonComponent title="Add 1" onPress={incrementQuantity} />
+      <ButtonComponent style={{ margin: 5 }} title="Remove 1" onPress={decrementQuantity} />
     </View>
   );
 }
